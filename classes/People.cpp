e5de6be834bf139f78fd5381aea7460116c85590
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2024-12-06 21:48:08 -0800

    v1.0: update people class to be more comprehensive

--- classes/People.cpp
+++ classes/People.cpp
@@ -1,49 +1,133 @@
-using namespace std;
+#include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
 #include "Item.cpp"
+using namespace std;
+
 class People {
-    public:
-        People(string name, int health, int energy, int mem, int money): name(name), health(health), energy(energy), mem(mem), money(money) {}
-        void setName(string name) {
-            this->name = name;
-        }
-        string getName() {
-            return name;
-        }
-        void setHealth(int health) {
-            this->health = health;
-        }
-        int getHealth() {
-            return health;
-        }
-        void setEnergy(int energy){
-            this->energy = energy;
-        }
-        int getEnergy() {
-            return energy;
-        }
-        void setMem(int mem) {
-            this->mem = mem;
-        }
-        int getMem() {
-            return mem;
-        }
-        void setMoney(int money) {
-            this->money = money;
-        }
-        int getMoney() {
-            return money;
-        }
-        // inventory items
+public:
+    string name;
+    int health;
+    int energy;
+    int mem;
+    int money;
 
-    private:
-        string name;
-        int health;
-        int energy;
-        int mem;
-        int money;
-        vector <Item> inventory;
-        // inventory items
+    People(string name, int health, int energy, int mem, int money)
+        : name(name), health(health), energy(energy), mem(mem), money(money) {}
 
+    void setName(string name) {
+        this->name = name;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    void modifyStats(int healthChange, int energyChange, int memChange, int moneyChange) {
+        health += healthChange;
+        energy += energyChange;
+        mem += memChange;
+        money += moneyChange;
+        if (health < 0) health = 0;
+        if (energy < 0) energy = 0;
+        if (money < 0) money = 0;
+    }
+
+    virtual void displayStats() {
+        cout << "Name: " << name << endl;
+        cout << "Health: " << health << endl;
+        cout << "Energy: " << energy << endl;
+        cout << "Memory: " << mem << endl;
+        cout << "Money: " << money << endl;
+    }
+};
+
+class Player : public People {
+private:
+    vector<Item> inventory;
+
+public:
+    Player(string name, int health, int energy, int mem, int money)
+        : People(name, health, energy, mem, money) {}
+
+    void addItem(const Item& item) {
+        inventory.push_back(item);
+    }
+
+    void removeItem(string itemName) {
+        for (auto it = inventory.begin(); it != inventory.end(); ++it) {
+            if (it->getName() == itemName) {
+                inventory.erase(it);
+                return;
+            }
+        }
+        cout << "Item not found in inventory!" << endl;
+    }
+
+    void useItem(string itemName) {
+        for (auto it = inventory.begin(); it != inventory.end(); ++it) {
+            if (it->getName() == itemName) {
+                cout << "Using item: " << itemName << endl;
+                modifyStats(it->getHealthEffect(), it->getEnergyEffect(), 0, 0);
+                inventory.erase(it);
+                return;
+            }
+        }
+        cout << "Item not found in inventory!" << endl;
+    }
+
+    void displayStats() override {
+        People::displayStats();
+        cout << "Inventory: ";
+        if (inventory.empty()) {
+            cout << "No items." << endl;
+        } else {
+            for (const auto& item : inventory) {
+                cout << item.getName() << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+class NPC : public People {
+public:
+    NPC(string name)
+        : People(name, 50, 0, 0, 0) {}
+
+    void attack(Player& player) {
+        int damage = rand() % 10 + 1;
+        cout << name << " attacks " << player.getName() << " for " << damage << " damage!" << endl;
+        player.modifyStats(-damage, 0, 0, 0);
+    }
+
+    void give(Player& player) {
+        int itemOrMoney = rand() % 2;
+        if (itemOrMoney == 0) {
+            int amount = rand() % 20 + 1;
+            cout << name << " gives " << amount << " money to " << player.getName() << "!" << endl;
+            player.modifyStats(0, 0, 0, amount);
+        } else {
+            Item gift("Health Potion", 20, 0);
+            cout << name << " gives a " << gift.getName() << " to " << player.getName() << "!" << endl;
+            player.addItem(gift);
+        }
+    }
+
+    void sendMessage() {
+        vector<string> messages = {
+            "Stay strong, traveler!",
+            "Beware of the cave ahead.",
+            "You can do it!",
+            "Remember to rest."
+        };
+        int randomIndex = rand() % messages.size();
+        cout << name << " says: \"" << messages[randomIndex] << "\"" << endl;
+    }
+
+    void displayStats() override {
+        cout << "Name: " << name << endl;
+        cout << "Health: " << health << endl;
+    }
 };
