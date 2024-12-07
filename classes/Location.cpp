7826e8750cbd0e8f7f2f1a19e2548b0ea9170f47
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2024-12-06 21:36:11 -0800

    v1.0: updated location and map

--- classes/Location.cpp
+++ classes/Location.cpp
@@ -1,3 +1,71 @@
-class Location {
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include "Item.cpp"
+using namespace std;
 
+class Location {
+public:
+    string name;
+    string description;
+    vector<Item> items;
+    map<string, Location*> connections;
+
+    Location(string locName, string locDesc)
+        : name(locName), description(locDesc) {}
+
+    void addConnection(string direction, Location* destination) {
+        connections[direction] = destination;
+    }
+
+    void display() {
+        cout << "Location: " << name << endl;
+        cout << description << endl;
+        if (!items.empty()) {
+            cout << "Items here: ";
+            for (const auto& item : items) {
+                cout << item << " ";
+            }
+            cout << endl;
+        }
+        cout << "Connections: ";
+        for (const auto& connection : connections) {
+            cout << connection.first << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Map {
+public:
+    vector<Location*> locations;
+
+    Location* addLocation(string name, string description) {
+        Location* newLocation = new Location(name, description);
+        locations.push_back(newLocation);
+        return newLocation;
+    }
+
+    Location* findLocation(string name) {
+        for (Location* loc : locations) {
+            if (loc->name == name) {
+                return loc;
+            }
+        }
+        return nullptr;
+    }
+
+    void displayMap() {
+        for (Location* loc : locations) {
+            loc->display();
+            cout << "--------------------" << endl;
+        }
+    }
+
+    ~Map() {
+        for (Location* loc : locations) {
+            delete loc;
+        }
+    }
 };
