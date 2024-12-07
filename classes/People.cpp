9311f9521134321608a1be06910f61242e3c9ed1
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2024-12-06 21:24:44 -0800

    v1.0: initalize player class with other things

---
+++ classes/People.cpp
@@ -0,0 +1,44 @@
+class People {
+    public:
+        People(string name, int health, int energy, int mem, int money): name(name), health(health), energy(energy), mem(mem), money(money) {}
+        void setName(string name) {
+            this->name = name;
+        }
+        string getName() {
+            return name;
+        }
+        void setHealth(int health) {
+            this->health = health;
+        }
+        int getHealth() {
+            return health;
+        }
+        void setEnergy(int energy){
+            this->energy = energy;
+        }
+        int getEnergy() {
+            return energy;
+        }
+        void setMem(int mem) {
+            this->mem = mem;
+        }
+        int getMem() {
+            return mem;
+        }
+        void setMoney(int money) {
+            this->money = money;
+        }
+        int getMoney() {
+            return money;
+        }
+        // inventory items
+
+    private:
+        string name;
+        int health;
+        int energy;
+        int mem;
+        int money;
+        // inventory items
+
+};
